be1c723483a3d9a5b7270c94e3900b6f429924e1
Jacob Pomatto <jdp0041@uah.edu>
2021-02-06 18:41:24 -0600

    Widget Tests for Alert Dialog

--- test/helper_test.dart
+++ test/helper_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:myportion_app/services/helper.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -22,4 +23,26 @@ void main() {
   group('Validate Mobile', () {});
 
   group('Validate Password', () {});
+
+  group('Notification Alert Tests', () {
+    testWidgets('Notification Alert Builds with Correct Text',
+        (WidgetTester tester) async {
+      Builder(builder: (BuildContext context) {
+        showAlertDialog(context, 'Title', 'Content');
+        expect(find.text('Title'), findsOneWidget);
+        expect(find.text('Content'), findsOneWidget);
+        expect(find.text('Does not Exist'), findsNothing);
+        return Placeholder();
+      });
+    });
+    testWidgets('Notification Alert Exists Only in Context',
+        (WidgetTester tester) async {
+      Builder(builder: (BuildContext context) {
+        showAlertDialog(context, 'Title', 'Content');
+        return Placeholder();
+      });
+      expect(find.text('Title'), findsNothing);
+      expect(find.text('Content'), findsNothing);
+    });
+  });
 }
