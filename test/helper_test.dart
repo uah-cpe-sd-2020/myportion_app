d4b211002f31504ed3ccd2331962b8f3e6376c04
Jacob Pomatto <jdp0041@uah.edu>
2021-02-06 19:21:36 -0600

    Widget Tests for Progress Dialog

--- test/helper_test.dart
+++ test/helper_test.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:myportion_app/services/helper.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:myportion_app/ui/signUp/SignUpScreen.dart';
+//import '../lib/ui/signUp/SignUpScreen.dart';
+//import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   group('Validate Name', () {
@@ -24,6 +27,49 @@ void main() {
 
   group('Validate Password', () {});
 
+  group('Progress Dialog Tests', () {
+    testWidgets('Progress Dialog Builds with Correct Text',
+        (WidgetTester tester) async {
+      Builder(builder: (BuildContext context) {
+        showProgress(context, 'Message', false);
+        expect(find.text('Message'), findsOneWidget);
+        expect(find.text('Does not Exist'), findsNothing);
+        return Placeholder();
+      });
+    });
+
+    testWidgets('Progress Dialog Exists Only in Context',
+        (WidgetTester tester) async {
+      Builder(builder: (BuildContext context) {
+        showProgress(context, 'Message', false);
+        return Placeholder();
+      });
+      expect(find.text('Title'), findsNothing);
+      expect(find.text('Content'), findsNothing);
+    });
+
+    testWidgets('Progress Dialog Update Progress', (WidgetTester tester) async {
+      Builder(builder: (BuildContext context) {
+        showProgress(context, 'Message', false);
+        expect(find.text('Message'), findsOneWidget);
+        updateProgress('NewMessage');
+        expect(find.text('Message'), findsNothing);
+        expect(find.text('NewMessage'), findsOneWidget);
+        return Placeholder();
+      });
+    });
+
+    testWidgets('Progress Dialog Hide Progress', (WidgetTester tester) async {
+      Builder(builder: (BuildContext context) {
+        showProgress(context, 'Message', false);
+        expect(find.text('Message'), findsOneWidget);
+        hideProgress();
+        expect(find.text('Message'), findsNothing);
+        return Placeholder();
+      });
+    });
+  });
+
   group('Notification Alert Tests', () {
     testWidgets('Notification Alert Builds with Correct Text',
         (WidgetTester tester) async {
@@ -45,4 +91,27 @@ void main() {
       expect(find.text('Content'), findsNothing);
     });
   });
+
+  /* group('Push Utilities', () {
+    testWidgets('Push Creates View',
+        (WidgetTester tester) async {
+      Builder(builder: (BuildContext context) {
+        push(context, new SignUpScreen);
+        expect(find.byWidget(SignUpScreen), findsNothing);
+        return Placeholder();
+      });
+    });
+  }); */
+  /* group('displayCircleImage Tests', () {
+    testWidgets('displayCircleImage has image', (WidgetTester tester) async {
+      await tester.pumpWidget(
+          displayCircleImage('/assets/images/placeholder.jpg', 20, false));
+      expectLater(find.byType(CachedNetworkImage),
+          matchesGoldenFile('../assets/images/placeholder.jpg'));
+    });
+    /* testWidgets('displayCircleImage has no image', (WidgetTester tester) async {
+      await tester.pumpWidget(displayCircleImage('', 20, false));
+      expect(find.byType(ClipOval), findsOneWidget);
+    }); */
+  }); */
 }
